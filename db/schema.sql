73bec1cfde074c5e3353076f4e41ba88ef53bba6
Varun Tanna <varun_tanna@ymail.com>
2022-06-23 15:30:32 -0400

    linked files and finished schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXIST employees;
+CREATE DATABASE employees;
+USE employees;
+
+CREATE TABLE department (
+    id INT NOT NULL PRIMARY KEY, 
+    department_name VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE employees (
+    id INT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL PRIMARY KEY,
+    title TEXT NOT NULL,
+    salary DECIMAL(10, 0) NOT NULL,
+    department_id INT NOT NULL,
+);
