da251cc1ca44640c65a805f5827c57a9d76d1b08
Varun Tanna <varun_tanna@ymail.com>
2022-06-23 19:32:32 -0400

    started inquire promt

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const consoleTable = require('console.table');
 
-const connection = mysql.createConnection(
-    {
-        host: 'localhost',
-        port: 3001,
-        user: 'root',
-        password: "",
-        database: 'employee_db',
-    },
-    console.log('Connected to the employees_db')
-);
-
-connection.query('SELECT * FROM employees', function(err, results) {
-    console.log(results)
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3001,
+    user: 'root',
+    password: "",
+    database: 'employee_db',
 });
+connection.connect((err) => {
+    if (err) throw err;
+
+    startSearch();
+});
+
+function startSearch() {
+    inquirer.prompt({
+        name: 'start',
+        type: 'list',
+        message: 'What would you like to choose?',
+        choices: [
+            "All Employees",
+            "Departments",
+            "Roles",
+            "Add Employee",
+            "Add Department",
+            "Add Role",
+            "Update Role",
+        ]
+    })
+}
+
+
+
+
+// connection.query('SELECT * FROM employees', function(err, results) {
+//     console.log(results)
+// });
+
+
