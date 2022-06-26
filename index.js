6e633e3d3870439ef305888da0bcbdc32ee8cf8b
Varun Tanna <varun_tanna@ymail.com>
2022-06-26 17:46:47 -0400

    adding swtich cases

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const Connection = require('mysql/lib/Connection');
 require('console.table');
 
 
@@ -14,6 +15,48 @@ db.connect(function (err) {
     if (err) throw err;
     startSearch();
 });
+
+function startSearch() {
+    inquirer.prompt({
+        message: 'What would you like to do?',
+        type: 'list',
+        choices: [
+            "Show All Emplyoees",
+            "Show All Roles",
+            "Show All Departments",
+            "Add Employee",
+            "Add Role",
+            "Add Department",
+            "QUIT",
+        ],
+        name: "choice"
+    }).then(answers => {
+        console.log(answers.choice);
+        switch (answers.choice) {
+            case "Show All Employees":
+                showAllEmployees()
+                break;
+            case "Show All Roles":
+                showAllRoles()
+                break;
+            case "Show All Departments":
+                showAllDepartments()
+                break;
+            case "Add Employee":
+                addEmployee()
+                break;
+            case "Add Role":
+                addRole()
+                break;
+            case "Add Department":
+                addDepartment();
+            default:
+                connection.end()
+                break;
+
+        }
+    })
+}
 
 const fn = {
     showAllEmployees() {
@@ -44,26 +87,26 @@ const fn = {
     },
 };
 
-const init = () => {
-    const choices = [
-        { name: 'show All Employees', value: 'showAllEmployees' },
-        { name: 'show All Roles', value: 'showAllRoles' },
-        { name: 'show All Departments', value: 'showAllDepartments' },
-        { name: 'Exit', value: 'exit' },
+// const init = () => {
+//     const choices = [
+//         { name: 'show All Employees', value: 'showAllEmployees' },
+//         { name: 'show All Roles', value: 'showAllRoles' },
+//         { name: 'show All Departments', value: 'showAllDepartments' },
+//         { name: 'Exit', value: 'exit' },
 
-    ];
+//     ];
 
-    inquirer.prompt([
-        {
-            type: 'rawlist',
-            name: 'query',
-            message: 'What option would you like to select?',
-            choices,
-        }
-    ]).then((answers) => fn[answers.query]());
-};
+//     inquirer.prompt([
+//         {
+//             type: 'rawlist',
+//             name: 'query',
+//             message: 'What option would you like to select?',
+//             choices,
+//         }
+//     ]).then((answers) => fn[answers.query]());
+// };
 
-init();
-  
+// init();
+
 
 
